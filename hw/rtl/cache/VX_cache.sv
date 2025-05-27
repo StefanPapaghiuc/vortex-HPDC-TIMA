b243109159feaa5339547508abb212ecb2c54078
StefanPapaghiuc <161493937+StefanPapaghiuc@users.noreply.github.com>
2025-05-27 14:05:52 +0200

    Update VX_cache.sv
    Banks to one

--- hw/rtl/cache/VX_cache.sv
+++ hw/rtl/cache/VX_cache.sv
@@ -20,11 +20,11 @@ module VX_cache import VX_gpu_pkg::*; #(
     parameter NUM_REQS              = 4,
 
     // Size of cache in bytes
-    parameter CACHE_SIZE            = 32768,
+    parameter CACHE_SIZE            = 8192,
     // Size of line inside a bank in bytes
     parameter LINE_SIZE             = 64,
     // Number of banks
-    parameter NUM_BANKS             = 4,
+    parameter NUM_BANKS             = 1,
     // Number of associative ways
     parameter NUM_WAYS              = 4,
     // Size of a word in bytes
