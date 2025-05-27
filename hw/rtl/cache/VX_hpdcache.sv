a5d831c1bfdf2475a996d68f70b01fe079075b88
StefanPapaghiuc <161493937+StefanPapaghiuc@users.noreply.github.com>
2025-05-27 12:43:35 +0200

    Update VX_hpdcache.sv
    Set banks to one, add MEM_PORTS

--- hw/rtl/cache/VX_hpdcache.sv
+++ hw/rtl/cache/VX_hpdcache.sv
@@ -23,12 +23,15 @@ module VX_hpdcache
     // Number of Word requests per cycle
     parameter NUM_REQS              = 4,
 
+    // Number of memory ports
+    parameter MEM_PORTS             = 1,
+    
     // Size of cache in bytes
-    parameter CACHE_SIZE            = 32768,
+    parameter CACHE_SIZE            = 8192,
     // Size of line inside a bank in bytes
     parameter LINE_SIZE             = 64,
     // Number of banks
-    parameter NUM_BANKS             = 4,
+    parameter NUM_BANKS             = 1,
     // Number of associative ways
     parameter NUM_WAYS              = 4,
     // Size of a word in bytes
