ffffe4247c770c95a579a4349de18190c87e98ab
StefanPapaghiuc <stefan.papaghiuc@grenoble-inp.org>
2025-07-11 11:09:12 +0200

    Default BLOCK_DIM to 2 (for 4 thread core)

--- tests/opencl/transpose/main.cc
+++ tests/opencl/transpose/main.cc
@@ -23,7 +23,7 @@
 #include "oclUtils.h"
 #include "shrQATest.h"
 
-#define BLOCK_DIM 4
+#define BLOCK_DIM 2
 
 // max GPU's to manage for multi-GPU parallel compute
 const unsigned int MAX_GPU_COUNT = 1;
